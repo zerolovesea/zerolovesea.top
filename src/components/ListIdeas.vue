ea9f435bf5d6d29729e2222b2d68793ec10459ea
yangzhou23 <yangzhou23@iflytek.com>
2025-06-12 23:26:24 +0800

    250612 optimizer idea post

--- src/components/ListIdeas.vue
+++ src/components/ListIdeas.vue
@@ -47,21 +47,20 @@ function formatDate(date: string): string {
       class="group mb-8 mt-[50px] flex flex-col gap-2"
       style="opacity: 1; transform: none;"
     >
+  
       <div class="flex gap-4">
-        <div class="flex flex-col items-center self-start md:flex-row md:gap-2">
-          <span class="self-start text-lg font-medium md:self-auto">{{ idea.author }}</span>
-          <span class="text-xs opacity-80 md:-translate-y-1 md:self-end">
-            {{ formatDate(idea.date) }}
-          </span>
+        <div class="flex flex-row items-center gap-2 text-base">
+          <span class="font-semibold whitespace-nowrap">{{ idea.author }}</span>
+          <span class="text-sm text-zinc-500 whitespace-nowrap">{{ formatDate(idea.date) }}</span>
         </div>
       </div>
-      <div class="min-w-0 max-w-full mt-2 pl-4 md:mt-0 md:-translate-y-4 md:pl-14">
+      <div class="min-w-0 max-w-full mt-0 pl-4 md:pl-14">
         <div class="relative w-full min-w-0">
           <div
             class="relative inline-block rounded-xl p-3 py-0 text-zinc-800 dark:text-zinc-200 rounded-tl-sm bg-zinc-600/5 dark:bg-zinc-500/20 max-w-full overflow-auto"
             style="margin-top: 15px; margin-left: -40px;"
             >
-            <div class="markdown_md__SEzck">
+            <div class="markdown_md__SEzck text-base">
               <p>{{ idea.content }}</p>
             </div>
           </div>
